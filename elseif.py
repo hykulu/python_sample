f93b6f81480d919ea8f90d3e58e8b6367ed4dfd0
KANNURU HYKULU <khykulu@gmail.com>
2025-06-27 19:23:06 +0530

    Add files via upload

---
+++ elseif.py
@@ -0,0 +1,14 @@
+#if condition:
+     #statements
+#else condition:
+    #statements
+#else:
+     #statements 
+a = 5
+b = 45
+if a<=33:
+    print("the if statement executed...")
+elif a>=35 and b<50:
+    print("the elif statement executed..")
+else:
+    print("the else statement executed...")
