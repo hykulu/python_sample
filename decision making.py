186175ec32b131f64224ef153a36ab4f6af0a07b
KANNURU HYKULU <khykulu@gmail.com>
2025-06-27 19:27:38 +0530

    Add files via upload

---
+++ decision making.py
@@ -0,0 +1,9 @@
+#decision making
+# if if else elif nestedif shorthand if e1
+# these are used to run the decisions
+
+# if condition:
+# statements
+n=int(input("enter a age..."))
+if n>=18:
+    print("the person is eligible",n)
