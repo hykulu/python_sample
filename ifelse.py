ebc643428f548285f159d5a5e4157f0ba30993d5
KANNURU HYKULU <khykulu@gmail.com>
2025-06-27 19:24:15 +0530

    Add files via upload

---
+++ ifelse.py
@@ -0,0 +1,11 @@
+#if condition:
+    #else:
+        #statement
+          #if same names:if statement will executed...
+          #if not same names than if else statement will be executed... 
+a = "ashu"
+b = "ashu"
+if a==b:
+    print("the if statement executed...")
+else:
+    print("the else statement executed...")
