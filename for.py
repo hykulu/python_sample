257c830acd23804ad81b909c320d23d641f59375
KANNURU HYKULU <khykulu@gmail.com>
2025-06-27 19:20:51 +0530

    Add files via upload

---
+++ for.py
@@ -0,0 +1,13 @@
+#for i in condition:
+     #statments
+# i is a variable
+# ai means condition
+# in is a 
+ #condition is either iterator
+for i in "artifical intelligence":
+    print(i)
+
+
+#range start stop skip)s^3
+for j in range(0,10):
+    print(j)
